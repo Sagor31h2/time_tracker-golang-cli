75b0cf9d8727e729c7c741eb4ee49234d9310d25
sagor31h2 <sagor31h2@gmail.com>
2024-07-07 19:29:54 +0600

    stop command

---
+++ cmd/stop.go
@@ -0,0 +1,33 @@
+/*
+Copyright © 2024 NAME HERE <EMAIL ADDRESS>
+*/
+package cmd
+
+import (
+	"fmt"
+	"time"
+
+	"github.com/spf13/cobra"
+)
+
+// stopCmd represents the stop command
+var stopCmd = &cobra.Command{
+	Short: "Stop tracking time",
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println("Stopped tracking time at:", time.Now().Format(time.RFC1123))
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(stopCmd)
+
+	// Here you will define your flags and configuration settings.
+
+	// Cobra supports Persistent Flags which will work for this command
+	// and all subcommands, e.g.:
+	// stopCmd.PersistentFlags().String("foo", "", "A help for foo")
+
+	// Cobra supports local flags which will only run when this command
+	// is called directly, e.g.:
+	// stopCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
